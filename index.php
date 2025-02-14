4e387c8fb7eff532ef657c002608497d65fc0626
Josh Robbs <josh@joshrobbs.com>
2025-02-14 18:53:39 -0500

    Add Carbon Fields package and initial configuration files

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: WPMB Toolkit
+ * Plugin URI: https://joshrobbs.com/wpmb-toolkit
+ * Description: A toolkit plugin for WordPress.
+ * Version: 0.1.0
+ * Author: Josh Robbs
+ * Author URI: https://joshrobbs.com
+ * License: BSD-3
+ * Text Domain: wpmb-toolkit
+ *
+ * @package WPMB_Toolkit
+ */
+
+namespace WPMB_Toolkit;
+
+defined( 'ABSPATH' ) || exit;
+
+require_once __DIR__ . '/vendor/autoload.php';
