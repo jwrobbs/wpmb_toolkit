4638fdffa7cd6651798fe7bc8e4c04e1a9e5e4ab
Josh Robbs <josh@joshrobbs.com>
2025-02-23 10:11:44 -0500

    Add WPMB Toolkit plugin implementation and initialization

---
+++ WPMBToolkit.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Plugin Name: WPMB Toolkit
+ * Plugin URI: https://joshrobbs.com/wpmb-toolkit
+ * Description: A toolkit plugin for WordPress devs and webmasters.
+ * Version: 0.1.0
+ * Author: Josh Robbs
+ * Author URI: https://joshrobbs.com
+ * License: BSD-3
+ * Text Domain: wpmb-toolkit
+ *
+ * @package WPMB_Toolkit
+ */
+
+namespace WPMB_Toolkit;
+
+use WPMB_Toolkit\Common\Hooks;
+
+defined( 'ABSPATH' ) || exit;
+
+
+/**
+ * Class WPMBToolkit.
+ */
+class WPMBToolkit {
+	/**
+	 * Path.
+	 *
+	 * @var string
+	 */
+	protected static $path;
+
+	/**
+	 * URL.
+	 *
+	 * @var string
+	 */
+	protected static $url;
+
+	/**
+	 * Is_init.
+	 *
+	 * @var bool
+	 */
+	protected static $is_init = false;
+
+	/**
+	 * Instance.
+	 *
+	 * @var WPMBToolkit
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Initialize the plugin.
+	 */
+	public static function init() {
+		if ( self::$is_init ) {
+			return;
+		}
+		self::$is_init = true;
+
+		require_once __DIR__ . '/vendor/autoload.php';
+		Hooks::register();
+
+		self::$path = plugin_dir_path( __FILE__ );
+		self::$url  = plugin_dir_url( __FILE__ );
+	}
+
+	/**
+	 * Get the plugin path.
+	 *
+	 * @return string
+	 */
+	public static function get_path() {
+		return self::$path;
+	}
+
+	/**
+	 * Get the plugin URL.
+	 *
+	 * @return string
+	 */
+	public static function get_url() {
+		return self::$url;
+	}
+
+	/**
+	 * Constructor.
+	 */
+	private function __construct() {
+	}
+}
+
+/**
+ * Initialize the plugin.
+ */
+WPMBToolkit::init();
